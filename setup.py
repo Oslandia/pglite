390a838cdd34bd6268dd4a2c4c2889f07221562b
Hugo Mercier <hugo.mercier@oslandia.com>
2017-08-14 16:09:17 +0200

    Version 1.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pglite',
-    version='1.0.3',
+    version='1.0.4',
     description="PostgreSQLite",
     long_description="PostgreSQLite - a simple postgresql cluster",
     classifiers=[
